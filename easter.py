24207db09903a75e97f852653fe13768b2956a7d
eiebehwiakwheh <jng33@srcf.net>
2025-05-05 08:44:11 +0100

    Create easter.py

---
+++ easter.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+def easter_date(year):
+    a = year % 19
+    b = year // 100
+    c = year % 100
+    d = b // 4
+    e = b % 4
+    f = (b + 8) // 25
+    g = (b - f + 1) // 3
+    h = (19 * a + b - d - g + 15) % 30
+    i = c // 4
+    k = c % 4
+    l = (32 + 2 * e + 2 * i - h - k) % 7
+    m = (a + 11 * h + 22 * l) // 451
+    month = (h + l - 7 * m + 114) // 31
+    day = ((h + l - 7 * m + 114) % 31) + 1
+    return f"{day:02d}.{month:02d}.{year}"
+
+if __name__ == "__main__":
+    year = datetime.now().year
+    print(f"Easter Sunday in {year} is on {easter_date(year)}")
