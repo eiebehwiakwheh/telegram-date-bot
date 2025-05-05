16665d5c8f8b0e6f35d4b190a24047223f7697aa
eiebehwiakwheh <jng33@srcf.net>
2025-05-05 08:42:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,20 @@
+# bot.py
 import os
+import logging
 from datetime import datetime
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
+logging.basicConfig(level=logging.INFO)
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
 async def reply_with_date(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    today = datetime.now().strftime('%d.%m.%Y')
-    await update.message.reply_text(today)
+    today = datetime.now().strftime('%Y-%m-%d')
+    await update.message.reply_text(f"Today's date is: {today}")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), reply_with_date))
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(os.environ['BOT_TOKEN']).build()
-    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), reply_with_date))
     app.run_polling()
